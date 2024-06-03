9362930a3febaff6aa5e119a9441152f14ae76ae
Antony Gradillas <50510640+avgra3@users.noreply.github.com>
2024-06-03 15:41:38 -0700

    Update to build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,12 +4,13 @@ set -e -x
 mkdir build
 cd build
 
-# cmake ${CMAKE_ARGS} \
-#     -DCMAKE_BUILD_TYPE=Release \
-#     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-#     ..
+ cmake ${CMAKE_ARGS} \
+     -DCMAKE_BUILD_TYPE=Release \
+     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+     ..
 
-make -k -j${CPU_COUNT}
+# make -k -j${CPU_COUNT}
 ctest --rerun-faild --output-on-failure --test-dir $SRC_DIR/build/unittest/libmariadb
 ctest --rerun-faild --output-on-failure --test-dir $SRC_DIR/build/unittest/mytap
-make install
+# make install
+cmake --build .
