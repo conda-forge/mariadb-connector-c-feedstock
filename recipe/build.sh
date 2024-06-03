f5d637b80f7066ebb8e59d15633f3ed831c203ba
Antony Gradillas <50510640+avgra3@users.noreply.github.com>
2024-06-03 15:40:06 -0700

    Test change to build system

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,10 +4,10 @@ set -e -x
 mkdir build
 cd build
 
-cmake ${CMAKE_ARGS} \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    ..
+# cmake ${CMAKE_ARGS} \
+#     -DCMAKE_BUILD_TYPE=Release \
+#     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+#     ..
 
 make -k -j${CPU_COUNT}
 ctest --rerun-faild --output-on-failure --test-dir $SRC_DIR/build/unittest/libmariadb
