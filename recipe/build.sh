441bbda656a0d58229500d2468766f5f0c6afc72
Antony <antonygradillas@gmail.com>
2024-08-22 08:44:51 -0700

    Updated build shell script to account for typo

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 set -e -x
 
 # For linux-ppc64le
-if [[ "$target_platform" == "linux-ppc64le" ]]; then
+if [[ "${target_platform}" == "linux-ppc64le" ]]; then
   # avoid error 'relocation truncated to fit: R_PPC64_REL24'
   export CFLAGS="$(echo ${CFLAGS} | sed 's/-fno-plt//g') -fplt"
   export CXXFLAGS="$(echo ${CXXFLAGS} | sed 's/-fno-plt//g') -fplt"
