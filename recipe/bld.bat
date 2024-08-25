f7ec95827175787920bc2d52fe56b92fe72ce1bb
Antony <antonygradillas@gmail.com>
2024-08-24 21:03:06 -0700

    Update to build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,23 +2,20 @@ mkdir build && cd build
 
 :: Set INSTALL_DOCREADMEDIR to a junk path to avoid installing the README into PREFIX
 cmake %CMAKE_ARGS% ^
-      -GNinja ^
+      ::-GNinja ^
       -DCMAKE_BUILD_TYPE="Release" ^
-      -DCMAKE_C_FLAGS="-I%LIBRARY_INC%" ^
-      -DCMAKE_CXX_FLAGS="-I%LIBRARY_INC%" ^
+      ::-DCMAKE_C_FLAGS="-I%LIBRARY_INC%" ^
       -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-      -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-      -DINSTALL_DOCREADMEDIR_STANDALONE="%cd%/junk" ^
-      -DINSTALL_DOCDIR="%cd%/junk" ^
+      ::-DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -DWITH_SSL=ON ^
       -DDEFAULT_SSL_VERIFY_SERVER_CERT=OFF ^
       ..
 
-cmake --build . --config RelWithDebInfo -j
+cmake --build . --config RelWithDebInfo -j --target install
 
-::if errorlevel 1 exit 1
+if errorlevel 1 exit 1
 ctest --rerun-failed --output-on-failure --test-dir %SRC_DIR%\build\unittest\libmariadb
 ctest --rerun-failed --output-on-failure --test-dir %SRC_DIR%\build\unittest\mytap
 
 cmake --install .
-::if errorlevel 1 exit 1
+if errorlevel 1 exit 1
