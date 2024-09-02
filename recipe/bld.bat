9e254bcc8ac0c7c67d651297314b49efabe85a09
avgra3 <antonygradillas@gmail.com>
2024-09-01 20:26:59 -0700

    Update to windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,11 +2,18 @@ mkdir build && cd build
 
 :: Set INSTALL_DOCREADMEDIR to a junk path to avoid installing the README into PREFIX
 cmake %CMAKE_ARGS% ^
-	-DCMAKE_BUILD_TYPE="Release" ^
-	-DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-	-DDEFAULT_SSL_VERIFY_SERVER_CERT=OFF ^
-	-DWITH_SSL=OPENSSL ^
-	 ..
+      -GNinja ^
+      -DCMAKE_BUILD_TYPE="Release" ^
+      -DCMAKE_C_FLAGS="-I%LIBRARY_INC%" ^
+      -DCMAKE_CXX_FLAGS="-I%LIBRARY_INC%" ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+      -DINSTALL_DOCREADMEDIR_STANDALONE="%cd%/junk" ^
+      -DINSTALL_DOCDIR="%cd%/junk" ^
+      -DWITH_SSL=ON ^
+      -DDEFAULT_SSL_VERIFY_SERVER_CERT=OFF ^
+      -DAUTH_GSSAPI=ON ^
+      ..
 
 cmake --build . --config RelWithDebInfo -j --target install
 
