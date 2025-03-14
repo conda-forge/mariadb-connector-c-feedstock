9429f883fc813d8e6719a1ce5e88106083155ad0
Antony G. <50510640+avgra3@users.noreply.github.com>
2025-03-14 11:38:14 -0700

    Add cross compile for OSX-ARM (#27)
    * Add cross compile
    
    * Updated for cross compiling
    
    * conda-smithy rerender
    
    * Added flag for osx-arm64 builds to prevent erroring out on compilation
    
    * Fix missing closing curly bracket on build.sh script
    
    * Treating warnings as warnings
    
    * Fixed flag for build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,19 +11,30 @@ fi
 mkdir build
 cd build
 
-cmake ${CMAKE_ARGS} \
+# -Wno-inline-asm => For osx-arm64, to prevent throwing an error on compilation.
+if [[ "${target_platform}" == "osx-arm64" ]]; then
+    cmake ${CMAKE_ARGS} \
+    -DWITH_EXTERNAL_ZLIB=ON \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DDEFAULT_SSL_VERIFY_SERVER_CERT=OFF \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCMAKE_COMPILE_WARNING_AS_ERROR=OFF \
+     ..
+else 
+    cmake ${CMAKE_ARGS} \
     -DWITH_EXTERNAL_ZLIB=ON \
     -DCMAKE_BUILD_TYPE=Release \
     -DDEFAULT_SSL_VERIFY_SERVER_CERT=OFF \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
      ..
+fi
 
 cmake --build . --config RelWithDebInfo -j --target install
 
 # Added for osx-arm
-# if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
-#   ctest --rerun-failed --output-on-failure --test-dir $SRC_DIR/build/unittest/libmariadb
-#   ctest --rerun-failed --output-on-failure --test-dir $SRC_DIR/build/unittest/mytap
-# fi
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
+   ctest --rerun-failed --output-on-failure --test-dir $SRC_DIR/build/unittest/libmariadb
+   ctest --rerun-failed --output-on-failure --test-dir $SRC_DIR/build/unittest/mytap
+fi
 
 cmake --install . --config RelWithDebInfo --prefix ${PREFIX}
